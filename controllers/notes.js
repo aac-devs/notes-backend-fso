64d067855d700325943e12256ad647a64f5cf764
Andrés <aac.devs@gmail.com>
2021-10-12 12:49:39 -0300

    users management

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,10 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
+const User = require('../models/user');
 
 // GET: READ ALL NOTES
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({});
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
   response.json(notes);
 });
 
@@ -20,12 +21,16 @@ notesRouter.get('/:id', async (request, response) => {
 // POST: CREATE NOTE
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
+  const user = await User.findById(body.userId);
   const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
+    user: user._id,
   });
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
   response.json(savedNote);
 });
 
